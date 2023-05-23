a3eac9b31cf7265b0caf70b177b719622d624ab7
giuseppevignanello <peppe.vignanello@gmail.com>
2023-05-23 16:11:54 +0200

    add header

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel Comics @yield('title') </title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -15,6 +18,7 @@
 <body>
 
     @include('partials.header')
+
 
     <main>
         @yield('content')
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,65 @@
+<header>
+    <div id="top_header">
+        <div class="container d-flex justify-content-end text-white">
+            <!-- fix ul -->
+            <ul class="d-flex">
+                <li class="me-5">
+                    DC POWER VISA
+                </li>
+                <li>
+                    ADDITIONAL DC SITES
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div id="bottom_header">
+        <div class="container">
+
+            <div class="d-flex justify-content-between align-items-center h-100">
+                <div class="logo">
+                    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+                </div>
+                <nav class="navbar">
+                    <ul class="d-flex">
+                        <li>
+                            <a> Characters </a>
+                        </li>
+                        <li>
+                            <a> Characters </a>
+                        </li>
+                        <li>
+                            <a> Characters </a>
+                        </li>
+                        <li>
+                            <a> Characters </a>
+                        </li>
+                        <li>
+                            <a> Characters </a>
+                        </li>
+                        <li>
+                            <a> Characters </a>
+                        </li>
+                        <li>
+                            <a> Characters </a>
+                        </li>
+                        <li>
+                            <a> Characters </a>
+                        </li>
+                        <li>
+                            <a> Characters </a>
+                        </li>
+                        <li>
+                            <a> Characters </a>
+                        </li>
+                        <li>
+                            <a> Characters </a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="input-group mb-3 searchBar">
+                    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Search" aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </div>
+            </div>
+        </div>
+</header>
